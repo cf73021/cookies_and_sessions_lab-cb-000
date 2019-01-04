1660286631621a88859c82ac1696d4cda8ff23d1
Chris Franklin <cf73021@gmail.com>
2019-01-04 15:29:10 +0000

    Automatically backed up by Learn

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ProductController, type: :controller do
 
   describe "GET #name:text" do
     it "returns http success" do
-      get :name:text
+      get :name,:text
       expect(response).to have_http_status(:success)
     end
   end
