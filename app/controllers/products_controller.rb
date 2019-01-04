93079d024a3d23806eea8e38306b8140f88a9506
Chris Franklin <cf73021@gmail.com>
2019-01-04 15:38:09 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     @product = Product.first
   end
+
+  def add
+    session[:cart] << params[:product]
+    @products = cart
+    redirect_to root_path
 end
